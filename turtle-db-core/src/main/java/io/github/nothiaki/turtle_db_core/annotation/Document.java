1359c9abf777ebb88ae702478e334117b3b2fa6f
nothiaki <celsohenrique367@gmail.com>
2025-04-14 13:28:14 -0300

    feat: annotations

---
+++ turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/annotation/Document.java
@@ -0,0 +1,12 @@
+package io.github.nothiaki.turtle_db_core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Document {
+  String dir();
+}
