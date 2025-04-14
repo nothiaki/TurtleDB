0b32d6c370c9c095da90f11e535cd2c281d8d628
nothiaki <celsohenrique367@gmail.com>
2025-04-14 17:41:17 -0300

    chore: git configs

--- turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/AtTimeLogic.java
+++ turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/AtTimeLogic.java
@@ -1,5 +1,7 @@
 package io.github.nothiaki.turtle_db_core;
 
 public enum AtTimeLogic {
+
   BEFORE, AFTER;
+
 }
--- turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/TurtleDbRepository.java
+++ turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/TurtleDbRepository.java
@@ -1,4 +1,12 @@
 package io.github.nothiaki.turtle_db_core;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TurtleDbRepository<T> {
+
+  T save(T entity);
+  List<T> find();
+  Optional<T> findOneById(String id);
+
 }
