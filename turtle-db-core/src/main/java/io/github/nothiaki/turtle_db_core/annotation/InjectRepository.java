c3f5895a4a5ad7a1eabf983a635d7076e977253e
nothiaki <celsohenrique367@gmail.com>
2025-05-03 21:20:13 -0300

    feat(core): inject repository annotation

---
+++ turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/annotation/InjectRepository.java
@@ -0,0 +1,10 @@
+package io.github.nothiaki.turtle_db_core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface InjectRepository {}
