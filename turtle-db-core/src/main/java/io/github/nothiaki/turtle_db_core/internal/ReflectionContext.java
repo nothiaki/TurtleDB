68f89b6ac8ba8a96d80c1cfbe21f111881e6f83a
nothiaki <celsohenrique367@gmail.com>
2025-05-15 22:57:36 -0300

    feat(core): scan interfaces implementation

--- turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/internal/ReflectionContext.java
+++ turtle-db-core/src/main/java/io/github/nothiaki/turtle_db_core/internal/ReflectionContext.java
@@ -3,11 +3,24 @@ package io.github.nothiaki.turtle_db_core.internal;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import io.github.nothiaki.turtle_db_core.TurtleDbRepository;
 
 public class ReflectionContext {
 
   public List<Class<?>> scanRepositoriesInterfaces(String basePackage) {
-    return new ArrayList<>();
+    Reflections reflections = new Reflections(basePackage, Scanners.TypesAnnotated, Scanners.SubTypes);
+
+    Set<Class<? extends TurtleDbRepository>> subTypes = reflections.getSubTypesOf(TurtleDbRepository.class);
+
+    return subTypes.stream()
+      .filter(Class::isInterface)
+      .collect(Collectors.toList());
   }
 
   public Object createRepositoryImpl(Class<?> repositoryInterface) {
